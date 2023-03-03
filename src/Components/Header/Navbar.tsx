ca98d2312fa2b1ba114f1669a4ce1550d45dfff7
H3XG0D <npc.legit@gmail.com>
2023-03-03 18:22:40 +0900

    first commit

---
+++ src/Components/Header/Navbar.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import '../Styles/Navbar.scss';
+import {Button} from "antd";
+
+export default function Navbar() {
+    const [slider, setSlider] = useState<boolean>(false);
+
+    let switcher = () => {
+        setSlider(!slider)
+    }
+
+    return (
+        <header className="header">
+
+            {/* ЛОГО Retaily */}
+            <div className="header-logo">
+                {/*<img src="./../Images/logo.svg" alt="Logo"/>*/}
+            </div>
+            <div className="header-switcher">
+                <div className={'header-switcher-item ' + (slider ? '' : 'header-switcher-item__active')} onClick={() => switcher()}>
+                    Поставщик
+                </div>
+                <div className={'header-switcher-item ' + (slider ? 'header-switcher-item__active' : '')} onClick={() => switcher()}>
+                    Торговая точка
+                </div>
+            </div>
+            {/* Меню */}
+            <div className="header-links">
+                <a href="/#" className="header-link__active">Стать партнером</a>
+                <a href="/#" className="header-link">О нас</a>
+                <a href="/#" className="header-link">Вакансии</a>
+                <a href="/#" className="header-link">Контакты</a>
+
+                {/* Отдельный компонент кнопки войти */}
+
+            </div>
+
+            <div>
+                <Button type="primary" style={{borderRadius: "20px", width: "110px"}}
+                        className="header-btn">Войти</Button>
+            </div>
+
+        </header>
+    );
+};
---
+++ src/Components/Partners/Content.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './../Styles/Content.scss'
+
+const Content = () => {
+    const title = "СОКРАЙЩАЙТЕ СВОИ ОПЕРАЦИОННЫЕ РАСХОДЫ И УВЕЛИЧИВАЙТЕ ПРИБЫЛЬ ВМЕСТЕ С RETAILY"
+
+    return (
+        <div className="main">
+            <div className="main-bg"></div>
+            <div className="main__wall"></div>
+            <div className="main-content">
+                <div className="main-content-title">
+                    <div className="main-content__title">{title}</div>
+                    <div className="main-content__subtitle">Автоматизируем прием заказов от торговых точек</div>
+                </div>
+                <div className="main-content__btn">
+                    <img src="" alt=""/>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Content;
